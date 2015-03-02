0681953c33702e1b71055686aef69bb83f35f5d2
ryanknights <hello@ryanknights.co.uk>
2015-03-02 19:21:08 +0000

    Testing heroku

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 "use strict";
 
-var express 		= require('express'),
-	app     		= express(),
-	server          = app.listen(8080),
-	io              = require('socket.io').listen(server);
+var express = require('express'),
+	app     = express(),
+	server  = app.listen(8080);
 
-var Twit = require('twit'),
-	T    = new Twit(
-	{
-	    consumer_key:         'jNPS3iRWmCaK9Ds2UFBjDqCZk',
-	    consumer_secret:      '9Mwj3VzPS9cjH0HzK83vPHTfnoLqPDW14XyhHFhYFXDDEjGfYl',
-	    access_token:         '581168903-0toGoNmEvbtYpNHs3nq7DgJTN7l2sYZbT8Aaggkq',
-	    access_token_secret:  'IJSzAVfZMGiHl83Ih55FkZNSWs8UMrGib4i90m64dzU8U'
-	});
+// var Twit = require('twit');
+
+// var T = new Twit(
+// {
+//     consumer_key:         'jNPS3iRWmCaK9Ds2UFBjDqCZk',
+//     consumer_secret:      '9Mwj3VzPS9cjH0HzK83vPHTfnoLqPDW14XyhHFhYFXDDEjGfYl',
+//     access_token:         '581168903-0toGoNmEvbtYpNHs3nq7DgJTN7l2sYZbT8Aaggkq',
+//     access_token_secret:  'IJSzAVfZMGiHl83Ih55FkZNSWs8UMrGib4i90m64dzU8U'
+// });
 
 app.use(express.static(__dirname + '/public'));
 
-//Setup socket.io functions
-require('./app/routes/socket.js')(io, T);
+// //Setup socket.io functions
+// require('./app/routes/socket.js')(io, T);
 
-app.use('/', require('./app/routes/default'));
+// app.use('/', require('./app/routes/default'));
 
 console.log('App listening on port 8080');
 
